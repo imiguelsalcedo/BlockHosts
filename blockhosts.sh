7de414991b5823ddc2e5950876de6d748463db84
Jose Maldonado (YukiteruAmano) <yukiteru.amano@gmx.com>
2015-05-29 12:17:13 -0430

    Agregadas nuevas listas de bloqueo. Agregada funcion de deduplicacion de entradas.

--- blockhosts.sh
+++ blockhosts.sh
@@ -14,6 +14,8 @@ if [ "$(id -u)" == "0" ]; then
 	wget -q http://www.malwaredomainlist.com/hostslist/hosts.txt -O malwaredomain.txt
 	wget -q http://winhelp2002.mvps.org/hosts.txt -O mvps.txt
 	wget -q http://someonewhocares.org/hosts/hosts -O someone.txt
+    wget -O yoyo_hosts.txt 'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext'
+    wget -q https://adaway.org/hosts.txt -O adway.txt
 
 	# Usamos sed para eliminar las lineas que no queremos
 	echo "Listas descargadas. Limpiando..."
@@ -31,18 +33,27 @@ if [ "$(id -u)" == "0" ]; then
 	sed -i '/#/d' malwaredomain.txt
 	sed -i '/localhost/d' malwaredomain.txt
 
+    sed -i "s/127.0.0.1/0.0.0.0/g" yoyo_hosts.txt
+    sed -i '/#/d' yoyo_hosts.txt
+    sed -i '/localhost/d' yoyo_hosts.txt 
+
+    sed -i "s/127.0.0.1/0.0.0.0/g" adway.txt 
+    sed -i '/#/d' adway.txt 
+    sed -i '/localhost/d' adway.txt 
+
 	# Recreamos el archivo hosts
 	echo "Recreando archivo /etc/hosts"
 	cat /etc/hosts.bak > hosts
-	cat malwaredomain.txt someone.txt mvps.txt >> hosts
-
-	# Movemos el nuevo archivo a su posicion final
+    cat yoyo_hosts.txt adway.txt malwaredomain.txt someone.txt mvps.txt > host_tmp
+    awk '!x[$0]++' host_tmp >> hosts
+	
+    # Movemos el nuevo archivo a su posicion final
 	echo "Copiando nuevo archivo /etc/hosts."
 	mv -v hosts /etc/hosts
 
 	# Borramos las listas originales
 	echo "Borrando temporales..."
-	rm malwaredomain.txt mvps.txt someone.txt
+	rm malwaredomain.txt mvps.txt someone.txt yoyo_hosts.txt adway.txt host_tmp 
 
 	# Proceso terminado
 	echo "Blockhost ha terminado. Archivo /etc/hosts creado. Saliendo."
