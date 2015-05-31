f23a394b0d69929a0f62ba1e76ecd8b0f4cc2322
Jose Maldonado (YukiteruAmano) <yukiteru.amano@gmx.com>
2015-05-31 03:08:51 -0430

    blockhosts.sh *************
    Cambios varios al script
    Ahora todo se hace con funciones mucho más sencillas
    Ahora todo el trabajo se hace como usuario y luego pasa a root
    Agregado opcion para agregar y borrar hosts
    Agregada la opcion de restaurar el hosts original en caso de problemas
    Agregada una funcion de deduplicacion de entradas
    
    README.md
    *********
    
    Actualización de instrucciones al nuevo script

--- README.md
+++ README.md
@@ -3,12 +3,12 @@ BlockHosts
 
 Un pequeño script bash para crear archivos /etc/hosts personalizados
 
-Este script usa las listas de MVPS, MalwareDomains y Someonecares 
-para crear un archivo hosts personalizado que bloquea buena parte de los
-dominios maliciosos y de publicidad de Internet. El script lleva
-consigo un sencillo instalador que a su vez automatiza todo el proceso
-inicial de configuración y realiza la creación inicial del hosts, 
-haciendo un respaldo del archivos hosts original.
+Este script usa las listas de MVPS, MalwareDomains, Someonecares
+Yoyo List y Adway para crear un archivo hosts personalizado que bloquea
+buena parte de los dominios maliciosos y de publicidad de Internet.
+El script lleva consigo un sencillo instalador que a su vez automatiza
+todo el proceso inicial de configuración y realiza la creación inicial
+del hosts, haciendo un respaldo del archivos hosts original.
 
 Para instalar el archivo solo necesita hacer el siguiente procedimiento:
 
@@ -18,16 +18,29 @@ cd BlockHosts
 
 sudo ./INSTALL
 
-Con eso ya tendrá instalado el script.
+Con eso ya tendrán instalado el script.
 
-Para ejecutar solo deben usar el comando
+Para ejecutar solo deben usar el comando:
 
-sudo blockhost.sh 
+blockhost.sh
 
-O hacerlo por la direccion completa
+O usando el path completo:
 
-sudo /usr/local/bin/blockhosts.sh
+/usr/local/bin/blockhosts.sh
 
-Para desinstalar solo hagan
+En ambos cosas solo se le pedirán permisos administrativos en el momento
+de acceder y modificar los archivos del sistema, el resto del proceso se
+hace como usuario normal.
+
+Entre las opciones del script están:
+
+* blockhosts.sh add hosts
+    Para agregar un hosts personalizado y bloquearlo
+* blockhosts.sh dell hosts
+    Para eliminar el bloqueo a un hosts en especifico.
+* blockhosts.sh restore
+    Para restaurar el archivo /etc/hosts a su estado original
+
+Para desinstalar solo deberán ejecutar
 
 sudo ./UNINSTALL
--- blockhosts.sh
+++ blockhosts.sh
@@ -1,64 +1,110 @@
 #!/bin/bash
 
-if [ "$(id -u)" == "0" ]; then
-    #Respaldamos el archivo original de /etc/hosts en /etc/hosts.bak
-    if [ ! -f /etc/hosts.bak ]; then
-        echo "Respaldo del /etc/hosts original"
-        cp /etc/hosts /etc/hosts.bak
-    fi
+BLOCK='0.0.0.0 '
 
-    # Descargamos las listas MVPS y MalwareDomain. 
-    # Para agregar otras listas debe crear los comandos necesarios
-    echo "Descargando listas..."
-    cd /tmp
-    wget -q http://www.malwaredomainlist.com/hostslist/hosts.txt -O malwaredomain.txt
-    wget -q http://winhelp2002.mvps.org/hosts.txt -O mvps.txt
-    wget -q http://someonewhocares.org/hosts/hosts -O someone.txt
-    wget -O yoyo_hosts.txt 'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext'
-    wget -q https://adaway.org/hosts.txt -O adway.txt
-
-    # Usamos sed para eliminar las lineas que no queremos
-    echo "Listas descargadas. Limpiando..."
-    sed -i "s/127.0.0.1/0.0.0.0/g" mvps.txt
-    sed -i '/#/d' mvps.txt
-    sed -i '/localhost/d' mvps.txt
-
-    sed -i "s/127.0.0.1/0.0.0.0/g" someone.txt
-    sed -i '/#/d' someone.txt
-    sed -i '/localhost/d' someone.txt
-    sed -i '/broadcasthost/d' someone.txt
-    sed -i '/local/d' someone.txt
-
-    sed -i "s/127.0.0.1/0.0.0.0/g" malwaredomain.txt
-    sed -i '/#/d' malwaredomain.txt
-    sed -i '/localhost/d' malwaredomain.txt
-
-    sed -i "s/127.0.0.1/0.0.0.0/g" yoyo_hosts.txt
-    sed -i '/#/d' yoyo_hosts.txt
-    sed -i '/localhost/d' yoyo_hosts.txt 
-
-    sed -i "s/127.0.0.1/0.0.0.0/g" adway.txt 
-    sed -i '/#/d' adway.txt 
-    sed -i '/localhost/d' adway.txt 
-
-    # Recreamos el archivo hosts
-    echo "Recreando archivo /etc/hosts"
-    cat /etc/hosts.bak > hosts
-    cat yoyo_hosts.txt adway.txt malwaredomain.txt someone.txt mvps.txt > host_tmp
-    awk '!x[$0]++' host_tmp >> hosts
-
-    # Movemos el nuevo archivo a su posicion final
-    echo "Copiando nuevo archivo /etc/hosts."
-    mv -v hosts /etc/hosts
-
-    # Borramos las listas originales
-    echo "Borrando temporales..."
-    rm malwaredomain.txt mvps.txt someone.txt yoyo_hosts.txt adway.txt host_tmp 
-
-    # Proceso terminado
-    echo "Blockhost ha terminado. Archivo /etc/hosts creado. Saliendo."
+# Funcion de inicializacion
+function init_blockhost() {
+if [ "$1" == "add" ]; then
+    add_hosts $2
+elif [ "$1" == "del" ]; then
+    del_hosts $2
+elif [ "$1" == "restore" ]; then
+    restore_hosts
 else
-    echo "Se necesitan permisos de root o usar sudo para ejecutar este script"
+    create_backup
+    create_blockhosts
 fi
+}
 
-exit 0
+# Respaldamos el archivo original /etc/hosts
+function create_backup() {
+if [ ! -f /etc/hosts.bak ]; then
+    echo "Realizando respaldo del /etc/hosts original"
+    echo "La operacion requiere de persmisos administrativos para ser realizada"
+    sudo bash -c "cp /etc/hosts /etc/hosts.bak"
+    echo "Repaldo realizado. Ubicacion: /etc/hosts.bak"
+fi
+}
+
+# Agregamos el bloqueo de un hosts
+function add_hosts() {
+if [ "$1" != "" ]; then
+    echo "Añadiendo host. Se requiere de permisos administrativos para realizar la operacion"
+    sudo bash -c "echo $BLOCK $1 >> /etc/hosts"
+else
+    echo "Argumento del hosts incorrecto. Verifique."
+    echo "Ej: blockhosts.sh add hosts-a-bloquear"
+fi
+}
+
+# Eliminamos el bloqueo de un hosts
+function del_hosts() {
+if [ $1 != "" ]; then
+    echo "Borrando host. Se requiere de permisos administrativos para realizar la operacion"
+    sudo bash -c "sed -i '/0.0.0.0 $1/d' /etc/hosts"
+else
+    echo "Argumento del hosts incorrecto. Verifique."
+    echo "Ej: blockhosts.sh del hosts-a-desbloquear"
+fi
+}
+
+function restore_hosts (){
+echo "Restaurando hosts original"
+echo "Se requieren permisos administrativos para realizar la operacion"
+sudo bash -c "cp /etc/hosts.bak /etc/hosts"
+}
+
+function sed_lists() {
+sed -i "s/127.0.0.1/0.0.0.0/g" $1
+sed -i '/#/d' $1
+sed -i '/localhost/d' $1
+sed -i '/broadcasthost/d' $1
+sed -i '/local/d' $1
+}
+
+function create_blockhosts() {
+# Descargamos las listas MVPS y MalwareDomain.
+# Para agregar otras listas debe crear los comandos necesarios
+echo "Descargando listas..."
+cd /tmp
+echo "Descargando: MalwareDomain. "
+wget -q http://www.malwaredomainlist.com/hostslist/hosts.txt -O malwaredomain.txt
+echo "Descargando: MVPS2002"
+wget -q http://winhelp2002.mvps.org/hosts.txt -O mvps.txt
+echo "Descargando: Someonewhocares"
+wget -q http://someonewhocares.org/hosts/hosts -O someone.txt
+echo "Descargando: Yoyo Lists"
+wget -q -O yoyo_hosts.txt 'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext'
+echo "Descargando: Adway Lists"
+wget -q https://adaway.org/hosts.txt -O adway.txt
+
+# Usamos sed para eliminar las lineas que no queremos
+echo "Listas descargadas. Limpiando..."
+sed_lists mvps.txt
+sed_lists someone.txt
+sed_lists malwaredomain.txt
+sed_lists yoyo_hosts.txt
+sed_lists adway.txt
+# Recreamos el archivo hosts
+echo "Recreando archivo /etc/hosts"
+fecha=`date`
+echo "# Lista de bloqueo por Blockhost" > hosts
+echo "# Generada en fecha: $fecha" >> hosts
+cat /etc/hosts.bak >> hosts
+cat yoyo_hosts.txt adway.txt malwaredomain.txt someone.txt mvps.txt > host_tmp
+awk '!x[$0]++' host_tmp >> hosts
+
+# Movemos el nuevo archivo a su posicion final
+echo "Copiando nuevo archivo /etc/hosts."
+echo "La operacion requiere de persmisos administrativos"
+sudo bash -c "mv -v hosts /etc/hosts"
+
+# Borramos las listas originales
+echo "Borrando temporales..."
+rm malwaredomain.txt mvps.txt someone.txt yoyo_hosts.txt adway.txt host_tmp
+
+# Proceso terminado
+echo "Blockhost ha terminado. Archivo /etc/hosts creado."
+}
+
+init_blockhost $1 $2
